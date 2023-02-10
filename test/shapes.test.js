b6d56e74745d3dbf69b292e6812ef0fafa1716e9
LearnedDr <fredrickj.chang@gmail.com>
2023-02-09 22:31:35 -0500

    finished the tests

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,25 +1,61 @@
 const Circle = require('../lib/circle.js')
+const Triangle = require('../lib/triangle.js');
+const Square = require('../lib/square.js');
 
 describe('Circle', () => {
+    const myCircle = new Circle('black', 'jlc', 'yellow', 'circle');
     describe('shape color', () => {
         it('should return the proper color of the shape', () => {
-            const myCircle = new Circle('black', 'jlc', 'yellow', 'circle');
             expect(myCircle.renderShapeColor()).toEqual('black');
-        })
+        });
+    });
+    describe('threeCharacters', () => {
+        it('should return the proper three characters', () => {
+            expect(myCircle.renderThreeCharacters()).toEqual('jlc');
+        });
+    });
+    describe('text color', () => {
+        it('should return the proper text color', () => {
+            expect(myCircle.renderCharacterColor()).toEqual('yellow');
+        });
+    });
+});
 
+describe('Triangle', () => {
+    const myTriangle = new Triangle('black', 'jlc', 'yellow', 'circle');
+    describe('shape color', () => {
+        it('should return the proper color of the shape', () => {
+            expect(myTriangle.renderShapeColor()).toEqual('black');
+        });
+    });
+    describe('threeCharacters', () => {
+        it('should return the proper three characters', () => {
+            expect(myTriangle.renderThreeCharacters()).toEqual('jlc');
+        });
+    });
+    describe('text color', () => {
+        it('should return the proper text color', () => {
+            expect(myTriangle.renderCharacterColor()).toEqual('yellow');
+        });
+    });
+});
 
-    })
-})
+describe('Square', () => {
+    const mySquare = new Square('black', 'jlc', 'yellow', 'circle');
+    describe('shape color', () => {
+        it('should return the proper color of the shape', () => {
+            expect(mySquare.renderShapeColor()).toEqual('black');
+        });
+    });
+    describe('threeCharacters', () => {
+        it('should return the proper three characters', () => {
+            expect(mySquare.renderThreeCharacters()).toEqual('jlc');
+        });
+    });
+    describe('text color', () => {
+        it('should return the proper text color', () => {
+            expect(mySquare.renderCharacterColor()).toEqual('yellow');
+        });
+    });
+});
 
-
-// A testing suite for Arithmetic is created.
-// describe('Arithmetic', () => {
-//     // A test is created to check that sum does in fact return the two numbers added together.
-//     describe('sum', () => {
-//       it('should take two numbers and add them together', () => {
-//         const total = 4;
-//         const arithmetic = new Arithmetic();
-//         expect(arithmetic.sum(2, 2)).toEqual(total);
-//       });
-//     });
-//   });
